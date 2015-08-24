16fac1d5a5b1c7496008f29ad0ddf80150371b5b
NoMemo <Wing.Of.War.1980th@Gmail.com>
2015-08-25 00:21:47 +0800

    Project init.

---
+++ GifAnimation2Code/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GifAnimation2Code
+//
+//  Created by JUE DUKE on 15/8/25.
+//  Copyright (c) 2015年 Nomemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
